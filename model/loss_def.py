f02ee20e66a4802518f8da1de36b178428890ab2
s-elo <zz321yy@qq.com>
2022-04-13 15:29:17 +0800

    a simple cycleGan finished

--- model/loss_def.py
+++ model/loss_def.py
@@ -23,15 +23,15 @@ def generator_fool_loss(generated):
     return loss_obj(tf.ones_like(generated), generated)
 
 
-def cycle_loss(real_image, cycled_image):
+def cycle_loss(real_image, cycled_image, lambda_cycle=LAMBDA):
     # minimize the loss between cycled img and original img
     loss1 = tf.reduce_mean(tf.abs(real_image - cycled_image))
 
     # make it more important
-    return LAMBDA * loss1
+    return lambda_cycle * loss1
 
 
-def identity_loss(real_image, same_image):
+def identity_loss(real_image, same_image, lambda_cycle=LAMBDA):
     # same_image == generator(real_image)
     loss = tf.reduce_mean(tf.abs(real_image - same_image))
-    return LAMBDA * 0.5 * loss
+    return lambda_cycle * 0.5 * loss
